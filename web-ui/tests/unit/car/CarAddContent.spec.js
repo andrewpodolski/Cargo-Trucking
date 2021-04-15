caad8c1a2ae0982fa62cd443863f923c923b055e
andrewpodolski <andrei.padolski@itechart-group.com>
2021-04-15 23:02:09 +0300

    CW-9 add vue testing utils and several tests for car component

---
+++ web-ui/tests/unit/car/CarAddContent.spec.js
@@ -0,0 +1,41 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import { Vuelidate } from 'vuelidate'
+
+import CarAddContent from '../../../src/components/content/car/CarAddContent'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+localVue.use(Vuelidate)
+const router = new VueRouter()
+
+describe('CarAddContent component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(CarAddContent, {
+      localVue,
+      router
+    })
+  })
+
+  it('renders correctly', async () => {
+    expect(wrapper.find('md-input[name="number"]')).toBeTruthy
+    expect(wrapper.find('md-input[name="fuelConsumption"]')).toBeTruthy
+    expect(wrapper.find('md-input[name="loadCapacity"]')).toBeTruthy
+    expect(wrapper.find('md-select')).toBeTruthy
+    expect(wrapper.find('md-card-actions').exists()).toBeTruthy
+  })
+
+  it('renders error messages when the form is empty', async () => {
+     await wrapper.setData({
+        number: 0
+     })
+    await wrapper.vm.$nextTick()
+
+    wrapper.vm.$emit('submit')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.findAll('.md-error').length).toBe(4)
+    expect(wrapper.find('.md-error').text()).toBe('Field must be maximum 10 characters')
+  })
+})
